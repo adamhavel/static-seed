4716e83186ca330dccb7a3075e70f722c054c48a
Adam Havel <havel.adam@gmail.com>
2014-02-05 23:56:46 +0100

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,394 @@
+module.exports = function(grunt) { grunt.initConfig({
+pkg: grunt.file.readJSON('package.json'),
+
+/* ==========================================================================
+   Variables
+   ========================================================================== */
+
+
+
+/* ==========================================================================
+   Configuration
+   ========================================================================== */
+
+/* JavaScript
+   ========================================================================== */
+
+jshint: {
+   options: {
+      jshintrc: 'grunt/.jshintrc',
+      force: true
+   },
+   client: [
+      'assets/site/js/**/*.js',
+      '!assets/site/js/**/*.min.js',
+   ]
+},
+
+concat: {
+   options: {
+      separator: ';'
+   },
+   bundle: {
+      src: [
+         'assets/site/lib/libs.min.js',
+         'assets/site/js/app.min.js'
+      ],
+      dest: 'assets/site/js/app.min.js',
+   }
+},
+
+uglify: {
+   app: {
+      options: {
+         report: 'min'
+      },
+      src: [
+         'assets/site/js/**/*.js',
+         '!assets/site/js/**/*.min.js',
+      ],
+      dest: 'assets/site/js/app.min.js'
+   },
+   libs: {
+      options: {
+         mangle: false
+      },
+      src: [
+         'assets/site/lib/**/*.min.js'
+      ],
+      dest: 'assets/site/lib/libs.min.js'
+   },
+   shiv: {
+      src: 'assets/site/lib/**/html5shiv.js',
+      dest: 'assets/site/js/html5shiv.min.js'
+   }
+},
+
+
+/* Stylesheet
+   ========================================================================== */
+
+sass: {
+   default: {
+      src: 'assets/site/css/default.scss',
+      dest: 'assets/site/css/default.css'
+   }
+},
+
+autoprefixer: {
+   default: {
+      options: {
+         browsers: ['> 1%', 'last 2 versions', 'firefox 24', 'opera 12.1']
+      },
+      src: 'assets/site/css/default.css'
+   }
+},
+
+remfallback: {
+   default: {
+      files: {
+         'assets/site/css/default.css': ['assets/site/css/default.css']
+      }
+   }
+},
+
+csslint: {
+   options: {
+      csslintrc: 'grunt/.csslintrc'
+   },
+   default: {
+      src: 'assets/site/css/default.css'
+   }
+},
+
+cssmin: {
+   options: {
+      report: 'min',
+      keepSpecialComments: 0
+   },
+   default: {
+      src: 'assets/site/css/default.css',
+      dest: 'assets/site/css/default.min.css'
+   }
+},
+
+uncss: {
+   options: {
+      ignore: ['.active'],
+      stylesheets: ['assets/site/css/default.css']
+   },
+   default: {
+      files: {
+         'assets/site/css/default.css': ['*.html']
+      }
+   }
+},
+
+
+/* Build
+   ========================================================================== */
+
+clean: {
+   build: ['build'],
+   temp: ['temp']
+},
+
+copy: {
+   build: {
+      files: [
+         {
+            expand: true,
+            src: [
+               '*.html',
+               'assets/**',
+               '!assets/site/lib/**',
+               '!assets/site/img/icon-*.svg',
+               '!assets/site/css/*.scss',
+               '!assets/site/css/default.css'
+            ],
+            dest: 'build/'
+         },
+      ]
+   },
+   icons: {
+      files: [
+         {
+            expand: true,
+            cwd: 'temp/bmp',
+            src: '*.png',
+            dest: 'assets/site/img',
+            rename: function (dest, src) {
+               return dest + '/icon-' + src;
+            }
+         },
+         {
+            expand: true,
+            cwd: 'temp',
+            src: '*.scss',
+            dest: 'assets/site/css'
+         }
+      ]
+   }
+},
+
+hashres: {
+   options: {
+      fileNameFormat: '${name}.${hash}.${ext}',
+   },
+   build: {
+      src: [
+         'build/assets/site/js/app.min.js',
+         'build/assets/site/css/default.min.css'
+      ],
+      dest: 'build/*.html'
+   }
+},
+
+/* Images
+   ========================================================================== */
+
+svgmin: {
+   build: {
+      files: [{
+         expand: true,
+         src: 'build/assets/site/img/*.svg'
+      }]
+   },
+   icons: {
+      files: [{
+         expand: true,
+         cwd: 'assets/site/img',
+         src: 'icon-*.svg',
+         dest: 'temp',
+         rename: function (dest, src) {
+            return dest + '/' + src.replace(/^icon-/, '');
+         }
+      }]
+   }
+},
+
+imagemin: {
+   png: {
+      options: {
+         optimizationLevel: 7
+      },
+      files: [{
+         expand: true,
+         src: 'build/assets/site/img/*.png'
+      }]
+   },
+   jpg: {
+      options: {
+         progressive: true
+      },
+      files: [{
+         expand: true,
+         src: 'build/assets/site/img/*.jpg'
+      }]
+   }
+},
+
+grunticon: {
+   icons: {
+      files: [{
+         expand: true,
+         cwd: 'temp',
+         src: ['*.svg'],
+         dest: 'temp'
+      }],
+      options: {
+         datasvgcss: '_icons.scss',
+         cssprefix: '.icon--',
+         pngfolder: 'bmp',
+         defaultWidth: 20,
+         defaultHeight: 20,
+         template: 'grunt/icon.hbs'
+      }
+   }
+},
+
+
+/* Runtime
+   ========================================================================== */
+
+watch: {
+   options: {
+      spawn: true
+   },
+   js: {
+      files: ['assets/site/js/**/*.js'],
+      tasks: ['makejs'],
+      options: {
+         livereload: true,
+         spawn: false
+      }
+   },
+   css: {
+      files: ['assets/site/css/default.css'],
+      tasks: ['makecss'],
+      options: {
+         livereload: true,
+         spawn: false
+      }
+   },
+   html: {
+      files: ['*.html'],
+      options: {
+         livereload: true,
+         spawn: false
+      }
+   },
+   icons: {
+      files: ['assets/site/img/icon-*.svg'],
+      tasks: ['makeicons']
+   }
+},
+
+nodemon: {
+   dev: {
+      options: {
+         file: 'server.js',
+         delayTime: 1,
+         env: {
+            PORT: 3000
+         }
+      }
+   }
+},
+
+exec: {
+   sass: {
+      cmd: 'sass --watch default.scss:default.css --style expanded',
+      cwd: 'assets/site/css'
+   }
+},
+
+concurrent: {
+   dev: ['exec:sass', 'nodemon:dev', 'watch'],
+   options: {
+      logConcurrentOutput: true
+   }
+}
+
+});
+
+
+/* ==========================================================================
+   Tasks
+   ========================================================================== */
+
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-csslint');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-nodemon');
+grunt.loadNpmTasks('grunt-concurrent');
+grunt.loadNpmTasks('grunt-autoprefixer');
+grunt.loadNpmTasks('grunt-remfallback');
+grunt.loadNpmTasks('grunt-uncss');
+grunt.loadNpmTasks('grunt-hashres');
+grunt.loadNpmTasks('grunt-exec');
+grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-svgmin');
+grunt.loadNpmTasks('grunt-grunticon');
+
+/* Helper tasks
+   ========================================================================== */
+
+grunt.registerTask('makecss', function(option) {
+   grunt.task.run('csslint');
+   if (option === 'build') {
+      grunt.task.run('uncss');
+   }
+   grunt.task.run([
+      /*'remfallback',*/ 'autoprefixer', 'cssmin'
+   ]);
+});
+
+grunt.registerTask('makejs', [
+   'uglify:app', 'concat:bundle', 'jshint:client'
+]);
+
+grunt.registerTask('makeicons', function() {
+   if (grunt.file.expand('assets/site/img/icon-*.svg').length > 0) {
+      grunt.task.run([
+         'svgmin:icons', 'grunticon:icons', 'copy:icons', 'clean:temp'
+      ]);
+   } else {
+      grunt.log.writeln('No icons found.');
+   }
+});
+
+grunt.registerTask('init', function(option) {
+   if (option === 'build') {
+      grunt.task.run('makecss:build');
+   } else {
+      grunt.task.run('makecss');
+   }
+   grunt.task.run([
+      'uglify:libs', 'uglify:shiv', 'makejs', 'makeicons'
+   ]);
+});
+
+
+/* Main tasks
+   ========================================================================== */
+
+grunt.registerTask('default', function() {
+   grunt.option('force', true);
+   grunt.task.run([
+      'init', 'concurrent:dev'
+   ]);
+});
+
+grunt.option('force', true);
+grunt.registerTask('build', [
+   'init:build', 'clean:build', 'copy:build', 'hashres:build', 'svgmin:build', 'imagemin'
+]);
+
+};
---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.compress());
+app.use(express.static(path.resolve('.')));
+app.listen(80);
+console.log('Listening on port 80');
