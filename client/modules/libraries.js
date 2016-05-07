94b871b14b541b1acf314753bf48bee67ceba574
Adam Havel <adam.havel@protonmail.com>
2016-05-07 22:29:14 +0200

    modified

--- client/modules/libraries.js
+++ client/modules/libraries.js
@@ -1,10 +1,4 @@
 export default [
-    // {
-    //     src: 'fastclick.js',
-    //     callback: function() {
-    //         FastClick.attach(document.body);
-    //     }
-    // },
     {
         src: 'smoothscroll.js',
         test: !('scrollBehavior' in document.documentElement.style)
